8869fc7cdbe5402b8b0a4fd821fc28be0d7da4e3
bruhhhz <23520258@gm.uit.edu.vn>
2025-10-23 14:52:23 +0700

    test

---
+++ Bai2/Bai2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2
+{
+    public partial class Bai2 : Form
+    {
+        public Bai2()
+        {
+            InitializeComponent();
+        }
+
+        private void Bai2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
